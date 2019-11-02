81cdd6c6b6c722700e012eacba54fe7ef370edb3
Laurigit <irualgm@gmail.com>
2019-11-02 10:46:49 +0200

    aettu tekee new_deckiä

--- scripts/tab/tab_new_deck.R
+++ scripts/tab/tab_new_deck.R
@@ -18,6 +18,27 @@ new_row <- data.table(Nimi = input$pakka_nimi,
                       Manastack_name_url = ""
 
 )
+con <- connDB(con)
 dbWriteTable(con, "DECKS_DIM", new_row, row.names = FALSE, append = TRUE)
+required_data("ADM_DI_HIERARKIA")
+updateData("SRC_DECKS", ADM_DI_HIERARKIA, globalenv())
+})
+
+observeEvent(input$load_new_deck_MIDS, {
+  # con <- connDB(con)
+  # kortit <- readCardsFromTextArea(input$new_deck_text_area, con)
+  # required_data(c("STG_CARDS", "STG_DECKS_DIM"))
+  # #session <- NULL
+  # #session$user <- "Lauri"
+  # mysidet <- STG_DECKS_DIM[Omistaja_ID == str_sub(session$user, 1, 1) & Side == 1, Pakka_ID]
+  # my_latest_side_PFI <- STG_CARDS[Pakka_ID %in% mysidet, .(Pakka_form_ID = max(Pakka_form_ID)), by = Pakka_ID]
+  # my_latest_side_decklists <- STG_CA
+  # sidelaput <- STG_CARDS[Maindeck == 0]
+  # #get kortit from sides
+  #
+  #
+  # Free_draft_ID <- dbQ("SELECT MAX(DRAFT_CARDS_ID) FROM CARDS")
+
+
 })
 
