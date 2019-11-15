1db161424a0d7effdae41b7bc1168debf3b6f78b
Laurigit <irualgm@gmail.com>
2019-11-15 20:27:15 +0200

    new idea where I check from name which MID is used in DB

--- scripts/solution_functions/readCardsFromTextArea.R
+++ scripts/solution_functions/readCardsFromTextArea.R
@@ -10,33 +10,27 @@ readCardsFromTextArea <- function(textAreaInput_text, con, STG_CARDS_DIM) {
 
 
 
-  taulu <- data.table(read.csv(text = textAreaInput_text, sep = ";", dec = ",", header = FALSE))
-  setnames(taulu, c("V1", "V2"), c("Name", "MID"))
+  taulu_all <- data.table(read.csv(text = textAreaInput_text, sep = ";", dec = ",", header = FALSE))
+  setnames(taulu_all, c("V1", "V2"), c("Name", "MID"))
 
-  #fix missing MIDs
- # tulos <- getCard_from_SF("Aethersnipe")[,MID]
-  try_res <- tryCatch({
-    taulu[MID == 0, MID := getCard_from_SF(Name, STG_CARDS_DIM)[,MID], by = Name]
-  }, error = function(e) {
-    "ERROR"
-  })
+ #  #fix missing MIDs
+ # # tulos <- getCard_from_SF("Aethersnipe")[,MID]
+ #  try_res <- tryCatch({
+ #    taulu[MID == 0, MID := getCard_from_SF(Name, STG_CARDS_DIM)[,MID], by = Name]
+ #  }, error = function(e) {
+ #    "ERROR"
+ #  })
+
+  #find the mid in the DB
+  sscols_cards_dim <- STG_CARDS_DIM[, .(Name, MID)]
+  taulu_all[, MID := NULL]
+  taulu <- sscols_cards_dim[taulu_all, on = "Name"]
 
 
-if (try_res[[1]] != "ERROR") {
-  MIDs <- STG_CARDS_DIM
-  new_cards <- setdiff(taulu[, Name], MIDs[, Name])
-  for (x in new_cards) {
-    res <- getCard_from_SF(x)
-    dbIoU("CARDS_DIM", res, con)
-
-  }
-  required_data("ADM_DI_HIERARKIA")
-  updateData("SRC_CARDS", ADM_DI_HIERARKIA, globalenv())
   more_info <- dbQ(paste0("SELECT Name, Converted_Cost, Colors from CARDS_DIM"),  con)
   join_more <- more_info[taulu, on = "Name"]
 
 return(join_more)
-} else {
-  return(try_res)
-}
+
+
 }
